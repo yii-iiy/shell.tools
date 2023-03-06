c4750ce49d9f7736bdf5a3e566b4eb02056133df
yii iiy <126449437+yii-iiy@users.noreply.github.com>
2023-03-06 13:45:17 +0800

    Create uwu.sh

---
+++ uwu.toy/uwu.sh
@@ -0,0 +1,75 @@
+#! /usr/bin/env sh
+
+echo '
+       ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## 
+    ##                                                                                 ## 
+   ##                                                                                   ## 
+  ##    uwu uwu         uwu uwu   uwu uwu           uwu uwu   uwu uwu         uwu uwu    ## 
+ ##     uwu uwu         uwu uwu   uwu uwu           uwu uwu   uwu uwu         uwu uwu     ## 
+ ##     uwu uwu         uwu uwu   uwu uwu  uwu uwu  uwu uwu   uwu uwu         uwu uwu     ## 
+ ##     uwu uwu         uwu uwu    uwu uwu uwu uwu uwu uwu    uwu uwu         uwu uwu     ## 
+ ##     uwu uwu         uwu uwu    uwu uwu u uwu wu wu uwu    uwu uwu         uwu uwu     ## 
+ ##     uwu uwu         uwu uwu    uwu uwu u uwu uwu u uwu    uwu uwu         uwu uwu     ## 
+ ##      uwu uwu       uwu uwu      uwu uwu uwu u uwu wu       uwu uwu       uwu uwu      ## 
+  ##      uwu uwu uwu uwu uwu       uwu uwu uwu wu uwu u        uwu uwu uwu uwu uwu      ## 
+   ##       uwu uwu uwu uwu            uwu uwu  uwu uwu           uwu uwu uwu uwu       ## 
+    ##                                                                                 ## 
+      ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ' &&
+echo &&
+
+while read -p 'uwu || ' -- spoken ;
+do 
+    case "$spoken" 
+    in 
+        '') ;; 
+        *) 
+            echo -e "$spoken" |
+                xargs -i -- echo 'OWO !! {}' &&
+        : ;; 
+    esac ;
+done &&
+
+echo '
+      ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## 
+    ##                                                                                 ## 
+   ##                                                                                   ## 
+  ##        OWO OWO OWO OWO     OWO OWO           OWO OWO         OWO OWO OWO OWO        ## 
+ ##       OWO OWO OWO OWO OWO    OWO OWO           OWO OWO      OWO OWO OWO OWO OWO       ## 
+ ##      OWO OWO       OWO OWO   OWO OWO    OWO    OWO OWO     OWO OWO       OWO OWO      ## 
+ ##     OWO OWO         OWO OWO   OWO OWO   OWO     OWO OWO   OWO OWO         OWO OWO     ## 
+ ##     OWO OWO         OWO OWO   OWO OWO    OWO    OWO OWO   OWO OWO         OWO OWO     ## 
+ ##     OWO OWO         OWO OWO   OWO OWO    OWO    OWO OWO   OWO OWO         OWO OWO     ## 
+ ##     OWO OWO         OWO OWO   OWO OWO  OWO OWO  OWO OWO   OWO OWO         OWO OWO     ## 
+ ##     OWO OWO         OWO OWO    OWO OWO OWO OWO OWO OWO    OWO OWO         OWO OWO     ## 
+ ##     OWO OWO         OWO OWO    OWO OWO O OWO WO WO OWO    OWO OWO         OWO OWO     ## 
+ ##     OWO OWO         OWO OWO    OWO OWO O OWO OWO O OWO    OWO OWO         OWO OWO     ## 
+ ##      OWO OWO       OWO OWO      OWO OWO OWO O OWO WO       OWO OWO       OWO OWO      ## 
+  ##      OWO OWO OWO OWO OWO       OWO OWO OWO WO OWO O        OWO OWO OWO OWO OWO      ## 
+   ##       OWO OWO OWO OWO            OWO OWO  OWO OWO           OWO OWO OWO OWO       ## 
+    ##                                                                                 ## 
+       ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ' &&
+echo &&
+
+:;
+
+
+
+# while read -p 'uwu || ' -- s;do case "$s" in '');;*)echo 'OWO !! '"$s";;esac;done
+
+
+       ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## 
+    ##                                                                                 ## 
+   ##                                                                                   ## 
+  ##    UWU UWU         UWU UWU   UWU UWU           UWU UWU   UWU UWU         UWU UWU    ## 
+ ##     UWU UWU         UWU UWU   UWU UWU           UWU UWU   UWU UWU         UWU UWU     ## 
+ ##     UWU UWU         UWU UWU   UWU UWU  UWU UWU  UWU UWU   UWU UWU         UWU UWU     ## 
+ ##     UWU UWU         UWU UWU    UWU UWU UWU UWU UWU UWU    UWU UWU         UWU UWU     ## 
+ ##     UWU UWU         UWU UWU    UWU UWU U UWU WU WU UWU    UWU UWU         UWU UWU     ## 
+ ##     UWU UWU         UWU UWU    UWU UWU U UWU UWU U UWU    UWU UWU         UWU UWU     ## 
+ ##      UWU UWU       UWU UWU      UWU UWU UWU U UWU WU       UWU UWU       UWU UWU      ## 
+  ##      UWU UWU UWU UWU UWU       UWU UWU UWU WU UWU U        UWU UWU UWU UWU UWU      ## 
+   ##       UWU UWU UWU UWU            UWU UWU  UWU UWU           UWU UWU UWU UWU       ## 
+    ##                                                                                 ## 
+      ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## 
+
+   
